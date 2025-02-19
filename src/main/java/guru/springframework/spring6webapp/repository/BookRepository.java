a77bf32967914fecc8673ed00acd99d18bf25a3d
roy.batista <roy.batista@timwe.com>
2025-02-19 15:30:52 +0000

    Correct Book repository name

--- src/main/java/guru/springframework/spring6webapp/repository/BookRepository.java
+++ src/main/java/guru/springframework/spring6webapp/repository/BookRepository.java
@@ -2,5 +2,5 @@ package guru.springframework.spring6webapp.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface Book extends CrudRepository<Book,Long> {
+public interface BookRepository extends CrudRepository<BookRepository,Long> {
 }
